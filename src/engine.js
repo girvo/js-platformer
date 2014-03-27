acf8791d94cab7bd60b25486af2698fcb503ecbb
Josh Girvin <josh.girvin@gmail.com>
2014-03-27 21:44:24 +1000

    Got the RAF working correctly

--- src/engine.js
+++ src/engine.js
@@ -3,8 +3,20 @@
  */
  
 function Engine() {
+    this.canvas = null;
 }
 
 Engine.prototype.run = function() {
+    var ctx = this.canvas.getContext('2d');
+    ctx.fillRect(0,0,100,100);
+    window.requestAnimationFrame(this.run.bind(this));
+}
 
+Engine.prototype.createCanvas = function(width, height) {
+    var canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.border = '1px solid black';
+    this.canvas = canvas;
+    return canvas;
 }
